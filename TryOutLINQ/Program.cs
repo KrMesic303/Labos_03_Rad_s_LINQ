4402343599407a919eab751243bb3fc7f9907f5c
Kristian Mesic <kristian.mesic303@gmail.com>
2023-11-28 18:49:27 +0100

    Add project files.

---
+++ TryOutLINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace TryOutLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primjeri: 
+            //Primjer 1.
+
+            int[] brojevi =
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28
+            };
+            var rezUpita = from num in brojevi
+                           where num > 10
+                           select num;
+            foreach (var rez in rezUpita)
+            {
+                Console.WriteLine(rez.ToString() + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
